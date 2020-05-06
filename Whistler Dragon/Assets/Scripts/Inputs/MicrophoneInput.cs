7a2e332189c850c15c2d8d69fa089a2cf61a043d
Merkado13 <Merkado13@users.noreply.github.com>
2020-05-06 15:59:47 +0200

    Microphone Detection

---
+++ Whistler Dragon/Assets/Scripts/Inputs/MicrophoneInput.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MicrophoneInput : MonoBehaviour
+{
+    [SerializeField]
+    private string selectedDevice;
+
+    private AudioSource audioSource;
+
+    private Slider microSlider;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(Microphone.devices.Length > 0)
+        {
+            selectedDevice = Microphone.devices[0].ToString();
+            audioSource.clip = Microphone.Start(selectedDevice, true, 10, AudioSettings.outputSampleRate);
+            audioSource.Play();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //audioSource.clip.
+    }
+}
