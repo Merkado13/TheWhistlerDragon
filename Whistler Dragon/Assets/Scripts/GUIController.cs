016e0c2ba4850fae4e82138f2e0d8b7ef059f74a
Merkado13 <Merkado13@users.noreply.github.com>
2020-05-06 16:41:12 +0200

    Mic Inputs and Shot

--- Whistler Dragon/Assets/Scripts/GUIController.cs
+++ Whistler Dragon/Assets/Scripts/GUIController.cs
@@ -1,14 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GUIController : MonoBehaviour
 {
     [SerializeField]
     private GameObject fireBar;
 
+    [SerializeField]
+    private Slider micSlider;
+
     public void UpdateFireBar(float xScale)
     {
         fireBar.transform.localScale = new Vector3(xScale, 1, 1);
+    }
+
+    public void UpdateMicSlider(float micVolume)
+    {
+        micSlider.value = micVolume * 100;
     }
 }
--- Whistler Dragon/Assets/Scripts/Inputs/KeyboardControls.cs
+++ Whistler Dragon/Assets/Scripts/Inputs/KeyboardControls.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class KeyboardControls : MonoBehaviour, IInputedControls
 {
+    [SerializeField]
+    private MicrophoneInput micInput;
+
+    public bool usingMic = true;
+
+    private const float MIC_TREESHOLD_DB = -50;
+
     public Quaternion GetPlayerRotation(Quaternion rotation, float rotationSpeed)
     {
         Vector3 rot = rotation.eulerAngles;
@@ -27,7 +34,14 @@ public class KeyboardControls : MonoBehaviour, IInputedControls
 
     public bool isShotInputActivated()
     {
-        return Input.GetKey(KeyCode.Space);
+        if (!usingMic)
+        {
+            return Input.GetKey(KeyCode.Space);
+        }
+        else
+        {
+            return micInput.MicLoudnessinDecibels >= MIC_TREESHOLD_DB;
+        }
     }
 
 }
