d677bcebac5bb883abb264245a54c3d35475339f
Andrea Ro <andriusky0210@hotmail.com>
2020-05-10 22:09:13 +0200

    Rotation of the dragon based on camera movement, changes in scene

---
+++ Whistler Dragon/Assets/Scripts/CamaraRotation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamaraRotation : MonoBehaviour
+{
+    [SerializeField]
+    Camera camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.rotation = camera.transform.rotation;
+    }
+}
