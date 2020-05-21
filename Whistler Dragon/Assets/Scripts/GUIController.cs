4cda29a0c3bdf481cd1eab03b170eb12e4e0e7f6
Merkado13 <Merkado13@users.noreply.github.com>
2020-05-21 13:39:40 +0200

    Pause Menu Button

--- Whistler Dragon/Assets/Scripts/GUIController.cs
+++ Whistler Dragon/Assets/Scripts/GUIController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GUIController : MonoBehaviour
 {
@@ -11,6 +12,16 @@ public class GUIController : MonoBehaviour
     [SerializeField]
     private Slider micSlider;
 
+    [SerializeField]
+    private GameObject menuPanel;
+
+    [SerializeField]
+    private Text gameState;
+    [SerializeField]
+    private GameObject resumeButton;
+    [SerializeField]
+    private GameObject restartButton;
+
     public void UpdateFireBar(float xScale)
     {
         fireBar.transform.localScale = new Vector3(xScale, 1, 1);
@@ -19,5 +30,37 @@ public class GUIController : MonoBehaviour
     public void UpdateMicSlider(float micVolume)
     {
         micSlider.value = micVolume / 3000;
+    }
+
+    public void GoToScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void ActivatePauseMenu()
+    {
+        gameState.text = "Pausa";
+        menuPanel.SetActive(true);
+        resumeButton.SetActive(true);
+        restartButton.SetActive(false);
+    }
+
+    public void DeactivatePauseMenu()
+    {
+        menuPanel.SetActive(false);
+    }
+
+    public void GameOver()
+    {
+        gameState.text = "Derrota";
+        resumeButton.SetActive(false);
+        restartButton.SetActive(true);
+    }
+
+    public void Victory()
+    {
+        gameState.text = "GameOver";
+        resumeButton.SetActive(false);
+        restartButton.SetActive(true);
     }
 }
